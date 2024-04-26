09ab688b664dc7f225c50f5cdfef33ba0a9e6827
Alejandro Pérez Durán <aperez@datamart.cl>
2024-04-26 10:08:33 -0400

    Update GithubUserForm component with client-side validation and toast message

--- components/github-user-form.tsx
+++ components/github-user-form.tsx
@@ -1,11 +1,24 @@
+"use client";
+import { toast } from "sonner";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export default function GithubUserForm() {
   return (
     <form className="flex gap-4" action={"/"}>
-      <Input placeholder="Nombre de usuario" name="user" />
-      <Button type="submit">Buscar</Button>
+      <Input
+        onChange={() => console.log("input")}
+        placeholder="Nombre de usuario"
+        name="user"
+      />
+      <Button
+        type="submit"
+        onClick={() => {
+          toast("Buscando usuario...");
+        }}
+      >
+        Buscar
+      </Button>
     </form>
   );
 }
